e30a2c6f023c65f3087203bfe114f7eccdf308cc
Fabian8abc <119813548+Fabian8abc@users.noreply.github.com>
2026-07-30 14:54:29 -0600

    Add files via upload
    v1

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'picking-pwa-v2';
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./'])));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
